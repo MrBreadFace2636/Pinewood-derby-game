e830acd3531a3cccca433dd19c42520951a7ffe5
MrBreadFace2636 <157673290+MrBreadFace2636@users.noreply.github.com>
2024-02-24 15:18:17 -0900

    add transition from splash scene to player count scene

---
+++ Scenes/Splash/Splash.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+var play_count_scene = preload("res://Scenes/PlayerCount/PlayerCount.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	var tree = get_tree()
+	tree.change_scene_to_packed(play_count_scene)
+	
