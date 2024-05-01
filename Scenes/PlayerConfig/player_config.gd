9f34be710ab079ebf4ca421d090e40c678fd3069
MrBreadFace2636 <157673290+MrBreadFace2636@users.noreply.github.com>
2024-04-30 20:29:02 -0800

    debugging

--- Scenes/PlayerConfig/player_config.gd
+++ Scenes/PlayerConfig/player_config.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 @export var player_number : int
 @onready var player_name = %PlayerName
 @onready var next_button = %NextButton
+@onready var bodies_button = %BodiesButton
+@onready var wheels_button = %WheelsButton
+@onready var aero_button = %AeroButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +13,9 @@ func _ready():
 	
 	if player_number == Globals.player_count:
 		next_button.text = "Race!"
+		
+	bodies_button.button_pressed = true
+	print(bodies_button.button_group.get_pressed_button())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -34,4 +40,9 @@ func _on_next_button_pressed():
 		scene_path = "res://Scenes/Race/Race.tscn"
 	else: 
 		scene_path = str("res://Scenes/PlayerConfig/player_", player_number + 1, "_config.tscn")
+	print(bodies_button)
+	print(bodies_button.button_group)
+	print(bodies_button.button_group.get_pressed_button())
+	print(bodies_button.button_group.get_pressed_button().get_meta("ButtonIndex"))
 	tree.change_scene_to_file(scene_path)
+	
